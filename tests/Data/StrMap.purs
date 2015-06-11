e9d20732584380b43b9cae611a9a7f2b5f055fef
sharkdp <davidpeter@web.de>
2015-06-11 12:04:00 +0200

    Update tests for 0.7

--- tests/Data/StrMap.purs
+++ tests/Data/StrMap.purs
@@ -2,15 +2,16 @@ module Tests.Data.StrMap where
 
 import Prelude
 
-import Data.Array (groupBy, map, sortBy)
+import Data.List (List(..), groupBy, sortBy, singleton, toList, zipWith)
 import Data.Foldable (foldl)
 import Data.Function (on)
 import Data.Maybe (Maybe(..))
-import Data.Int (fromNumber)
-import Data.Tuple (Tuple(..), fst, zip)
-import Debug.Trace
+import Data.Tuple (Tuple(..), fst)
+import Control.Monad.Eff.Console (log)
+import Test.Data.List
 import Test.QuickCheck ((<?>), quickCheck, quickCheck')
 import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
+import Test.QuickCheck.Gen (Gen(..))
 import qualified Data.String as S
 import qualified Data.StrMap as M
 
@@ -34,79 +35,79 @@ instance arbInstruction :: (Arbitrary v) => Arbitrary (Instruction String v) whe
       false -> do
         return (Delete k)
 
-runInstructions :: forall v. [Instruction String v] -> M.StrMap v -> M.StrMap v
+runInstructions :: forall v. List (Instruction String v) -> M.StrMap v -> M.StrMap v
 runInstructions instrs t0 = foldl step t0 instrs
   where
   step tree (Insert k v) = M.insert k v tree
   step tree (Delete k) = M.delete k tree
 
-number :: Number -> Number
+number :: Int -> Int
 number n = n
 
 strMapTests = do
-  trace "Test inserting into empty tree"
+  log "Test inserting into empty tree"
   quickCheck $ \k v -> M.lookup k (M.insert k v M.empty) == Just (number v)
     <?> ("k: " ++ show k ++ ", v: " ++ show v)
 
-  trace "Test delete after inserting"
+  log "Test delete after inserting"
   quickCheck $ \k v -> M.isEmpty (M.delete k (M.insert k (number v) M.empty))
     <?> ("k: " ++ show k ++ ", v: " ++ show v)
 
-  trace "Insert two, lookup first"
+  log "Insert two, lookup first"
   quickCheck $ \k1 v1 k2 v2 -> k1 == k2 || M.lookup k1 (M.insert k2 (number v2) (M.insert k1 (number v1) M.empty)) == Just v1
     <?> ("k1: " ++ show k1 ++ ", v1: " ++ show v1 ++ ", k2: " ++ show k2 ++ ", v2: " ++ show v2)
 
-  trace "Insert two, lookup second"
+  log "Insert two, lookup second"
   quickCheck $ \k1 v1 k2 v2 -> M.lookup k2 (M.insert k2 (number v2) (M.insert k1 (number v1) M.empty)) == Just v2
     <?> ("k1: " ++ show k1 ++ ", v1: " ++ show v1 ++ ", k2: " ++ show k2 ++ ", v2: " ++ show v2)
 
-  trace "Insert two, delete one"
+  log "Insert two, delete one"
   quickCheck $ \k1 v1 k2 v2 -> k1 == k2 || M.lookup k2 (M.delete k1 (M.insert k2 (number v2) (M.insert k1 (number v1) M.empty))) == Just v2
     <?> ("k1: " ++ show k1 ++ ", v1: " ++ show v1 ++ ", k2: " ++ show k2 ++ ", v2: " ++ show v2)
 
-  trace "Lookup from empty"
-  quickCheck $ \k -> M.lookup k (M.empty :: M.StrMap Number) == Nothing
+  log "Lookup from empty"
+  quickCheck $ \k -> M.lookup k (M.empty :: M.StrMap Int) == Nothing
 
-  trace "Lookup from singleton"
-  quickCheck $ \k v -> M.lookup k (M.singleton k (v :: Number)) == Just v
+  log "Lookup from singleton"
+  quickCheck $ \k v -> M.lookup k (M.singleton k (v :: Int)) == Just v
 
-  trace "Random lookup"
-  quickCheck' (fromNumber 5000) $ \instrs k v ->
+  log "Random lookup"
+  quickCheck' 5000 $ \instrs k v ->
     let
-      tree :: M.StrMap Number
+      tree :: M.StrMap Int
       tree = M.insert k v (runInstructions instrs M.empty)
     in M.lookup k tree == Just v <?> ("instrs:\n  " ++ show instrs ++ "\nk:\n  " ++ show k ++ "\nv:\n  " ++ show v)
 
-  trace "Singleton to list"
-  quickCheck $ \k v -> M.toList (M.singleton k v :: M.StrMap Number) == [Tuple k v]
+  log "Singleton to list"
+  quickCheck $ \k v -> M.toList (M.singleton k v :: M.StrMap Int) == singleton (Tuple k v)
 
-  trace "toList . fromList = id"
+  log "toList . fromList = id"
   quickCheck $ \arr -> let f x = M.toList (M.fromList x)
-                       in f (f arr) == f (arr :: [Tuple String Number]) <?> show arr
+                       in f (f arr) == f (arr :: List (Tuple String Int)) <?> show arr
 
-  trace "fromList . toList = id"
+  log "fromList . toList = id"
   quickCheck $ \m -> let f m = M.fromList (M.toList m) in
-                     M.toList (f m) == M.toList (m :: M.StrMap Number) <?> show m
+                     M.toList (f m) == M.toList (m :: M.StrMap Int) <?> show m
 
-  trace "fromListWith const = fromList"
+  log "fromListWith const = fromList"
   quickCheck $ \arr -> M.fromListWith const arr ==
-                       M.fromList (arr :: [Tuple String Number]) <?> show arr
+                       M.fromList (arr :: List (Tuple String Int)) <?> show arr
 
-  trace "fromListWith (<>) = fromList . collapse with (<>) . group on fst"
+  log "fromListWith (<>) = fromList . collapse with (<>) . group on fst"
   quickCheck $ \arr ->
     let combine (Tuple s a) (Tuple t b) = (Tuple s $ b <> a)
-        foldl1 g (x : xs) = foldl g x xs
+        foldl1 g (Cons x xs) = foldl g x xs
         f = M.fromList <<< (<$>) (foldl1 combine) <<<
             groupBy ((==) `on` fst) <<< sortBy (compare `on` fst) in
-    M.fromListWith (<>) arr == f (arr :: [Tuple String String]) <?> show arr
+    M.fromListWith (<>) arr == f (arr :: List (Tuple String String)) <?> show arr
 
-  trace "Lookup from union"
+  log "Lookup from union"
   quickCheck $ \m1 m2 k -> M.lookup k (M.union m1 m2) == (case M.lookup k m1 of
     Nothing -> M.lookup k m2
     Just v -> Just (number v)) <?> ("m1: " ++ show m1 ++ ", m2: " ++ show m2 ++ ", k: " ++ show k ++ ", v1: " ++ show (M.lookup k m1) ++ ", v2: " ++ show (M.lookup k m2) ++ ", union: " ++ show (M.union m1 m2))
 
-  trace "Union is idempotent"
-  quickCheck $ \m1 m2 -> (m1 `M.union` m2) == ((m1 `M.union` m2) `M.union` (m2 :: M.StrMap Number)) <?> (show (M.size (m1 `M.union` m2)) ++ " != " ++ show (M.size ((m1 `M.union` m2) `M.union` m2)))
+  log "Union is idempotent"
+  quickCheck $ \m1 m2 -> (m1 `M.union` m2) == ((m1 `M.union` m2) `M.union` (m2 :: M.StrMap Int)) <?> (show (M.size (m1 `M.union` m2)) ++ " != " ++ show (M.size ((m1 `M.union` m2) `M.union` m2)))
 
-  trace "toList = zip keys values"
-  quickCheck $ \m -> M.toList m == zip (M.keys m) (M.values m :: Array Number)
+  log "toList = zip keys values"
+  quickCheck $ \m -> M.toList m == zipWith Tuple (toList $ M.keys m) (M.values m :: List Int)
--- tests/Tests.purs
+++ tests/Tests.purs
@@ -2,15 +2,15 @@ module Tests where
 
 import Prelude
 
-import Debug.Trace
+import Control.Monad.Eff.Console (log)
 import Test.QuickCheck
 
 import Tests.Data.Map (mapTests)
 import Tests.Data.StrMap (strMapTests)
 
 main = do
-  trace "Running Map tests"
+  log "Running Map tests"
   mapTests
 
-  trace "Running StrMap tests"
+  log "Running StrMap tests"
   strMapTests
