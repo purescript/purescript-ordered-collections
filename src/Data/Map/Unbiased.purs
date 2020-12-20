21b21a11a8ce3629e3c0f48a58f7d844024c6e36
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-19 17:44:54 -0800

    Copy Internal, Gen, and main Map module to new Unbiased folder

---
+++ src/Data/Map/Unbiased.purs
@@ -0,0 +1,13 @@
+module Data.Map
+  ( module Data.Map.Internal
+  , keys
+  ) where
+
+import Prelude
+
+import Data.Map.Internal (Map, alter, catMaybes, checkValid, delete, empty, filter, filterKeys, filterWithKey, findMax, findMin, foldSubmap, fromFoldable, fromFoldableWith, fromFoldableWithIndex, insert, insertWith, isEmpty, isSubmap, lookup, lookupGE, lookupGT, lookupLE, lookupLT, member, pop, showTree, singleton, size, submap, toUnfoldable, toUnfoldableUnordered, union, unionWith, unions, intersection, intersectionWith, difference, update, values, mapMaybeWithKey, mapMaybe)
+import Data.Set (Set)
+import Unsafe.Coerce (unsafeCoerce)
+
+keys :: forall k v. Map k v -> Set k
+keys = (unsafeCoerce :: Map k Unit -> Set k) <<< void
