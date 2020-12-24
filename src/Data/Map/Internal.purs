cee5474fd003b332583fa5738f1c7d55e623e23f
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-24 09:28:30 -0800

    Use long-form names for SemigroupMap's type parameters in docs

--- src/Data/Map/Internal.purs
+++ src/Data/Map/Internal.purs
@@ -159,8 +159,8 @@ instance traversableWithIndexMap :: TraversableWithIndex k (Map k) where
           <*> f k2 v2
           <*> traverseWithIndex f right
 
--- | `SemigroupMap k v` provides a `Semigroup` instance for `Map` whose definition
--- | depends on the underlying `value` type's `Semigroup` instance.
+-- | `SemigroupMap key value` provides a `Semigroup` instance for `Map` whose
+-- | definition depends on the underlying `value` type's `Semigroup` instance.
 -- | You should only use this type when you need `Data.Map` to have
 -- | a `Semigroup` instance.
 -- |
